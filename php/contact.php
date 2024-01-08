d71481efa22f3c124ceb1be0fe0c728e8159cb93
Wepper <120311438+aj4nl@users.noreply.github.com>
2024-01-08 10:40:19 +0100

    Add files via upload

---
+++ php/contact.php
@@ -0,0 +1,46 @@
+<?php
+$errorMSG = "";
+// NAME
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is missing.";
+} else {
+    $name = $_POST["name"];
+}
+// EMAIL
+if (empty($_POST["email"])) {
+    $errorMSG .= "Email is missing.";
+} else {
+    $email = $_POST["email"];
+}
+// MESSAGE
+if (empty($_POST["message"])) {
+    $errorMSG .= "Message is missing.";
+} else {
+    $message = $_POST["message"];
+}
+$EmailTo = "info@kogna.tech";
+$Subject = "KOGNA - Message received.";
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+?>
